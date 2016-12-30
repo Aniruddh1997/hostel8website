60147a471deb2e41982289953d5a5a9921e4b801
aniruddh shetty <aniruddh shetty>
2016-12-30 16:06:33 +0530

    portals

--- js/main.js
+++ js/main.js
@@ -24,10 +24,12 @@ $(document).ready(function(){
 	if($("#dummy_header").is(":visible")){
 		$("#main_header")[0].style.top = "51px";
 		$("#main_header img")[0].style.top = "-5px";
+		$("#main_header")[0].style.opacity = "1";
 	}
 	else{
 		$("#main_header")[0].style.top = "0px";
 		$("#main_header img")[0].style.top = "0px";
+		$("#main_header")[0].style.opacity = "0.95";
 	}
 
 	//the dummy header disappears while resizing
@@ -36,10 +38,12 @@ $(document).ready(function(){
 			if($("#dummy_header").is(":visible")){
 				$("#main_header")[0].style.top = "51px";
 				$("#main_header img")[0].style.top = "-5px";
+				$("#main_header")[0].style.opacity = "1";
 			}
 			else{
 				$("#main_header")[0].style.top = "0px";
 				$("#main_header img")[0].style.top = "0px";
+				$("#main_header")[0].style.opacity = "0.95";
 			}
 		}
 		if($("#main_header .collapse.in").is(":visible")){
@@ -60,6 +64,7 @@ $(document).ready(function(){
 			if($(window).scrollTop() >= 51){
 				$("#main_header")[0].style.top = "0px";
 				$("#main_header img")[0].style.top = "0px";
+				$("#main_header")[0].style.opacity = "0.95";
 
 				$(".nav_headers").each(function(){
 					$(this)[0].style.paddingTop = '12px';
@@ -80,6 +85,8 @@ $(document).ready(function(){
 				var height = 51 - $(window).scrollTop();
 				$("#main_header")[0].style.top = height + "px";
 				$("#main_header img")[0].style.top = "-5px";
+				$("#main_header")[0].style.opacity = "1";
+
 				$(".nav_headers").each(function(){
 					$(this)[0].style.paddingTop = '25px';
 					$(this)[0].style.paddingBottom = '25px';
@@ -229,18 +236,18 @@ $(document).ready(function(){
 
     }
 
-    $(".upcoming_events .col-md-4").hover(
-    	function(){
-    		var elem = $(this);
-    		myVar = setTimeout(function(){
-    			elem.children(".overlay").animate({height:"50px"},150);
-    		}, 150)	
-    	},
-    	function(){
-    		$(this).children(".overlay").animate({height:"0px"},150);
-    		clearTimeout(myVar);
-    	}
-    )
+    // $(".upcoming_events .col-md-4").hover(
+    // 	function(){
+    // 		var elem = $(this);
+    // 		myVar = setTimeout(function(){
+    // 			elem.find(".overlay").animate({height:"100%",opacity:"0.9"},150);
+    // 		}, 150)	
+    // 	},
+    // 	function(){
+    // 		$(this).find(".overlay").animate({height:"0%",opacity:"0"},150);
+    // 		clearTimeout(myVar);
+    // 	}
+    // )
 
     $('#co_page').jqcarousel();
     $('#co_page').css("height", "500px");
@@ -249,7 +256,7 @@ $(document).ready(function(){
     	function(){
     		var elem = $(this);
     		myVar = setTimeout(function(){
-    			elem.children().velocity('fadeIn');
+    			elem.children().velocity('transition.shrinkIn');
     		}, 150)	
     	},
     	function(){
@@ -266,5 +273,143 @@ $(document).ready(function(){
 		}
 		carousel_click(i);
 	},2000)
+
+
+	$(function() {
+	    $(".upcoming_events .col-md-4>div").hover(function(e) {
+	    	var elem = $(this);
+	        var el_pos = $(this).offset();
+	        var edge = closestEdge(elem,e);
+
+	        $(this).find("span")[0].style.display = "block";
+	        if(edge == "left"){
+        		$(this).find(".overlay")[0].style.width = "0%";
+        		$(this).find(".overlay")[0].style.height = "100%";
+        		$(this).find(".overlay")[0].style.left = "0";
+        		$(this).find(".overlay")[0].style.right = "auto";
+        		$(this).find(".overlay").animate({width: "100%"},150);
+	        }
+	        else if(edge == "right"){
+        		$(this).find(".overlay")[0].style.width = "0%";
+        		$(this).find(".overlay")[0].style.height = "100%";
+        		$(this).find(".overlay")[0].style.right = "0";
+        		$(this).find(".overlay")[0].style.left = "auto";
+        		$(this).find(".overlay").animate({width: "100%"},150);
+        	}
+        	else if(edge == "top"){
+        		$(this).find(".overlay")[0].style.width = "100%";
+        		$(this).find(".overlay")[0].style.height = "0%";
+        		$(this).find(".overlay")[0].style.top = "0";
+        		$(this).find(".overlay")[0].style.bottom = "auto";
+        		$(this).find(".overlay").animate({height: "100%"},150);
+        	}
+        	else{
+        		$(this).find(".overlay")[0].style.width = "100%";
+        		$(this).find(".overlay")[0].style.height = "0%";
+        		$(this).find(".overlay")[0].style.bottom = "0";
+        		$(this).find(".overlay")[0].style.top = "auto";
+        		$(this).find(".overlay").animate({height: "100%"},150);
+	        }
+	    }, function(e) {
+	    	var elem = $(this);
+	        var el_pos = $(this).offset();
+	        var edge = closestEdge(elem,e);
+
+	       
+	        if(edge == "left"){
+        		$(this).find(".overlay")[0].style.left = "0";
+        		$(this).find(".overlay")[0].style.right = "auto";
+        		$(this).find(".overlay").animate({width: "0%"},150,function(){
+        			elem.find("span")[0].style.display = "none";
+        		});
+	        }
+	        else if(edge == "right"){
+        		$(this).find(".overlay")[0].style.right = "0";
+        		$(this).find(".overlay")[0].style.left = "auto";
+        		$(this).find(".overlay").animate({width: "0%"},150,function(){
+        			elem.find("span")[0].style.display = "none";
+        		});
+        	}
+        	else if(edge == "top"){
+        		$(this).find(".overlay")[0].style.top = "0";
+        		$(this).find(".overlay")[0].style.bottom = "auto";
+        		$(this).find(".overlay").animate({height: "0%"},150,function(){
+        			elem.find("span")[0].style.display = "none";
+        		});
+        	}
+        	else{
+        		$(this).find(".overlay")[0].style.bottom = "0";
+        		$(this).find(".overlay")[0].style.top = "auto";
+        		$(this).find(".overlay").animate({height: "0%"},150,function(){
+        			elem.find("span")[0].style.display = "none";
+        		});
+	        }
+
+	         // $(this).find(".overlay")[0].style.opacity = "0";
+	        
+	    });
+	});
+
+	// function closestEdge(x,y,w,h) {
+ //        var topEdgeDist = distMetric(x,y,w/2,0);
+ //        var bottomEdgeDist = distMetric(x,y,w/2,h);
+ //        var leftEdgeDist = distMetric(x,y,0,h/2);
+ //        var rightEdgeDist = distMetric(x,y,w,h/2);
+    
+ //        var min = Math.min(topEdgeDist,bottomEdgeDist,leftEdgeDist,rightEdgeDist);
+ //        switch (min) {
+ //            case leftEdgeDist:
+ //                return "left";
+ //            case rightEdgeDist:
+ //                return "right";
+ //            case topEdgeDist:
+ //                return "top";
+ //            case bottomEdgeDist:
+ //                return "bottom";
+	//     }
+	// }
+	    
+	// function distMetric(x,y,x2,y2) {
+	//     var xDiff = x - x2;
+	//     var yDiff = y - y2;
+	//     return (xDiff * xDiff) + (yDiff * yDiff);
+	// }
+
+	function closestEdge( elem, e ) {       
+
+            /** the width and height of the current div **/
+            var w = elem.width();
+            var h = elem.height();
+            var offset = elem.offset();
+            /** calculate the x and y to get an angle to the center of the div from that x and y. **/
+            /** gets the x value relative to the center of the DIV and "normalize" it **/
+            var x = (e.pageX - offset.left - (w/2)) * ( w > h ? (h/w) : 1 );
+            var y = (e.pageY - offset.top  - (h/2)) * ( h > w ? (w/h) : 1 );
+
+            /** the angle and the direction from where the mouse came in/went out clockwise (TRBL=0123);**/
+            /* first calculate the angle of the point, 
+             add 180 deg to get rid of the negative values
+             divide by 90 to get the quadrant
+             add 3 and do a modulo by 4  to shift the quadrants to a proper clockwise TRBL (top/right/bottom/left) */
+            var direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180 ) / 90 ) + 3 )  % 4;
+
+
+            /** do your animations here **/ 
+            switch(direction) {
+             case 0:
+                return 'top';
+             break;
+             case 1:
+                return 'right';
+             break;
+             case 2:
+                   return 'bottom';
+             break;
+             case 3:
+                   return 'left';
+             break;
+            }
+
+	}
 
 })
